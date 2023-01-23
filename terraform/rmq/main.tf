a7d9c1141596a3c43fe678b3b58153e5fe013d67
Chancesus <chancekgsmith@gmail.com>
2023-01-23 12:46:20 -0800

    fixed instance type error

--- terraform/rmq/main.tf
+++ terraform/rmq/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "rmq" {
   ami                    = "ami-07ebfd5b3428b6f4d"
-  instance_type          = "t2_micro"
+  instance_type          = "t2.micro"
   key_name               = "rabbitmq"
   vpc_security_group_ids = ["sg-0b27f815d2e3a2436"]
 
